bb3f19bb1b3055b5624446e82c80211651b09c8c
lessia2020 <63756124+lessia2020@users.noreply.github.com>
2023-01-28 15:47:02 +0100

    Add files via upload

---
+++ Exercise17.playground/Contents.swift
@@ -0,0 +1,37 @@
+//  Adding properties and methods to structures
+
+struct Car {
+    var color: String
+    var name: String
+    var topSpeed: Int
+    var mileage: Int
+    
+    func drive(){
+        print("\(color) car can drive at a maximum speed of: \(topSpeed).")
+    }
+}
+
+//Initializing a struct
+var newCar = Car(
+    color: "blue",
+    name: "Car 1",
+    topSpeed: 100,
+    mileage: 5000
+)
+
+var myCar = Car(
+    color: "pink",
+    name: "Car 2",
+    topSpeed: 100,
+    mileage: 4000
+)
+
+var smallCar = Car(
+    color: "orange",
+    name: "Car 3",
+    topSpeed: 80,
+    mileage: 3000
+)
+
+print("The color of the car is: \(myCar.color)")
+smallCar.drive()
